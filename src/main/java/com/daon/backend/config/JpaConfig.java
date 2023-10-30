591d0e71f8687f9ff7a06d1db6e1eb58427797ab
hi0yoo <yoo971202@naver.com>
2023-10-30 14:53:16 +0900

    init

---
+++ src/main/java/com/daon/backend/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.daon.backend.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public JPAQueryFactory queryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
