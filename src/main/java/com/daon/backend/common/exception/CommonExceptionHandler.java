5107b0d4571d62da033016b183215c35eaedd96e
hi0yoo <yoo971202@naver.com>
2023-11-04 01:41:07 +0900

    ❇️[feat] 검증 오류 응답 추가 #33

--- src/main/java/com/daon/backend/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/daon/backend/common/exception/CommonExceptionHandler.java
@@ -4,12 +4,37 @@ import com.daon.backend.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
 public class CommonExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
+        log.error("데이터 검증 오류");
+
+        BindingResult bindingResult = e.getBindingResult();
+        // 필드 오류 메시지들 하나로 묶기
+        String errorMessage = bindingResult.getFieldErrors().stream()
+                .map(this::parseFieldErrorMessage)
+                .collect(Collectors.joining(","));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.createError(errorMessage));
+    }
+
+    private String parseFieldErrorMessage(FieldError fieldError) {
+        String field = fieldError.getField();
+        String fieldErrorDefaultMessage = fieldError.getDefaultMessage();
+        return field + ":" + fieldErrorDefaultMessage;
+    }
 
     @ExceptionHandler(AbstractException.class)
     public ResponseEntity<ApiResponse<Void>> abstractExceptionHandle(AbstractException e) {
