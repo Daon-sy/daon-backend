3594505d177c2b131ff75fd78cc822eac59c4eaa
jaeyumn <wkddkgodsla@gmail.com>
2023-11-03 14:21:53 +0900

    ❇️[feat] 회원 도메인 예외 처리 추가 #29

--- src/main/java/com/daon/backend/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/daon/backend/common/exception/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package com.daon.backend.common.exception;
 
 import com.daon.backend.common.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
