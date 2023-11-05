7071dcebfa6b0362c1f7011dbe29253f1efb93f4
jaeyumn <wkddkgodsla@gmail.com>
2023-11-05 17:54:22 +0900

    🚧[setting] Swagger springdoc 설정

--- src/main/java/com/daon/backend/member/controller/MemberController.java
+++ src/main/java/com/daon/backend/member/controller/MemberController.java
@@ -4,6 +4,9 @@ import com.daon.backend.common.response.ApiResponse;
 import com.daon.backend.member.dto.SignInRequestDto;
 import com.daon.backend.member.dto.SignUpRequestDto;
 import com.daon.backend.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "Member", description = "Member domain API")
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
@@ -24,6 +28,10 @@ public class MemberController {
 
     private static final String BEARER_TYPE = "Bearer ";
 
+    @Operation(summary = "회원가입", description = "회원가입 요청입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "회원가입 성공")
+    })
     @PostMapping("/sign-up")
     public ApiResponse<Void> signUp(@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
         memberService.signUp(signUpRequestDto);
@@ -31,7 +39,10 @@ public class MemberController {
         return ApiResponse.createSuccess(null);
     }
 
-    // 응답 헤더에 접근해야 해서 예외적으로 ResponseEntity 사용
+    @Operation(summary = "로그인", description = "로그인 요청입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "로그인 성공")
+    })
     @PostMapping("/sign-in")
     public ResponseEntity<ApiResponse<Void>> signIn(@RequestBody @Valid SignInRequestDto signInRequestDto) {
         String token = memberService.signIn(signInRequestDto);
