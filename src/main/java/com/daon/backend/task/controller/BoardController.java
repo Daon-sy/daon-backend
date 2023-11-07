70fe02ed8c6d9b394f47f33c047d9503ca1f3824
jaeyumn <wkddkgodsla@gmail.com>
2023-11-07 09:48:14 +0900

    🔨[refactor] 보드 생성 api 스펙 추가 및 응답 코드 수정 #56

--- src/main/java/com/daon/backend/task/controller/BoardController.java
+++ src/main/java/com/daon/backend/task/controller/BoardController.java
@@ -4,7 +4,11 @@ import com.daon.backend.common.response.CommonResponse;
 import com.daon.backend.task.dto.request.CreateBoardRequestDto;
 import com.daon.backend.task.dto.response.CreateBoardResponseDto;
 import com.daon.backend.task.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,6 +18,11 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @Operation(summary = "보드 생성", description = "보드 생성 요청입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "보드 생성 성공")
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public CommonResponse<CreateBoardResponseDto> createBoard(@PathVariable("workspaceId") Long workspaceId,
                                                               @PathVariable("projectId") Long projectId,
