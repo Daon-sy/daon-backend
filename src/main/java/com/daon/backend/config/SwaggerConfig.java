62f8691f7ea76e9edaa06563c66b094526695637
jaeyumn <wkddkgodsla@gmail.com>
2023-11-12 15:54:16 +0900

    🔧[chore] API 문서 설정에 Token 인증 설정 추가 #88

--- src/main/java/com/daon/backend/config/SwaggerConfig.java
+++ src/main/java/com/daon/backend/config/SwaggerConfig.java
@@ -3,17 +3,24 @@ package com.daon.backend.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    final String JWT_SCHEME_NAME = "JWT Authentication";
+    final String SCHEME = "bearer";
+    final String BEARER_FORMAT = "JWT";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
     private Info apiInfo() {
@@ -23,4 +30,17 @@ public class SwaggerConfig {
                         "Request Dto 등 모든 필드에 부가 정보를 다는 것은 번거롭기도 하고 논의된 사항(시간 자원의 한계)에 맞지 않기 때문에 우선 제외했습니다.")
                 .version("1.0.0");
     }
+
+    private final SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList(JWT_SCHEME_NAME);
+
+    private final Components components = new Components()
+            .addSecuritySchemes(
+                    JWT_SCHEME_NAME,
+                    new SecurityScheme()
+                            .name(JWT_SCHEME_NAME)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme(SCHEME)
+                            .bearerFormat(BEARER_FORMAT)
+            );
 }
