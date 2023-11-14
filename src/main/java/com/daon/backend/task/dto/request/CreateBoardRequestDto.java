7f624c97e979583f804dfdc0b4486dda4301ca76
jaeyumn <wkddkgodsla@gmail.com>
2023-11-14 20:01:42 +0900

    🔨[refactor] 보드 API 수정 #109

--- src/main/java/com/daon/backend/task/dto/request/CreateBoardRequestDto.java
+++ src/main/java/com/daon/backend/task/dto/request/CreateBoardRequestDto.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class CreateBoardRequestDto {
 
     @NotBlank
+    @Size(max = 20)
     private String title;
 }
