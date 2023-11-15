3c4cae45f4d05565c9f70333f3f617dd2f687cbd
syeon27 <tktkrhk1027@gmail.com>
2023-11-15 15:10:13 +0900

    ❇️[feat] 워크스페이스 상세(단건) 조회 #74

---
+++ src/main/java/com/daon/backend/task/dto/workspace/FindWorkspaceResponseDto.java
@@ -0,0 +1,37 @@
+package com.daon.backend.task.dto.workspace;
+
+import com.daon.backend.task.domain.workspace.Workspace;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class FindWorkspaceResponseDto {
+
+    private Long workspaceId;
+
+    private String title;
+
+    private String description;
+
+    private String imageUrl;
+
+    private String division;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime modifiedAt;
+
+    public FindWorkspaceResponseDto(Workspace workspace) {
+        this.workspaceId = workspace.getId();
+        this.title = workspace.getTitle();
+        this.description = workspace.getDescription();
+        this.imageUrl = workspace.getImageUrl();
+        this.division = String.valueOf(workspace.getDivision());
+        this.createdAt = workspace.getCreatedAt();
+        this.modifiedAt = workspace.getModifiedAt();
+    }
+}
