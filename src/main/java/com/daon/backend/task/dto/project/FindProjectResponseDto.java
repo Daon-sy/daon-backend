ddbec3b4ce407460526791b83cae99ee13bf11b2
syeon27 <tktkrhk1027@gmail.com>
2023-11-15 14:11:58 +0900

    ❇️[feat] 프로젝트 상세(단건) 조회 #75

---
+++ src/main/java/com/daon/backend/task/dto/project/FindProjectResponseDto.java
@@ -0,0 +1,31 @@
+package com.daon.backend.task.dto.project;
+
+import com.daon.backend.task.domain.project.Project;
+import com.daon.backend.task.domain.task.Task;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class FindProjectResponseDto {
+
+    private Long projectId;
+
+    private String title;
+
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime modifiedAt;
+
+    public FindProjectResponseDto(Project project) {
+        this.projectId = project.getId();
+        this.title = project.getTitle();
+        this.createdAt = project.getCreatedAt();
+        this.modifiedAt = project.getModifiedAt();
+    }
+}
