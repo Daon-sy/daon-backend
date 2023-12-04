54c24796cf20ed96c042efbeba5fba03f601256c
sungeunnn <fronteun@naver.com>
2023-12-04 17:46:01 +0900

    ❇️[feat] 워크스페이스 공지사항을 위한 API 엔드포인트 추가 #219
    - WorkspaceNoticeController로 목록조회, 단건조회 API 엔드포인트 구현

--- src/main/java/com/daon/backend/task/controller/WorkspaceNoticeController.java
+++ src/main/java/com/daon/backend/task/controller/WorkspaceNoticeController.java
@@ -4,6 +4,8 @@ import com.daon.backend.task.domain.authority.Authority;
 import com.daon.backend.task.domain.authority.CheckRole;
 import com.daon.backend.task.dto.workspace.CreateWorkspaceNoticeRequestDto;
 import com.daon.backend.task.dto.workspace.CreateWorkspaceNoticeResponseDto;
+import com.daon.backend.task.dto.workspace.FindWorkspaceNoticeResponseDto;
+import com.daon.backend.task.dto.workspace.FindWorkspaceNoticesResponseDto;
 import com.daon.backend.task.service.WorkspaceNoticeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -33,5 +35,23 @@ public class WorkspaceNoticeController {
     public CreateWorkspaceNoticeResponseDto createWorkspaceNotice(@PathVariable Long workspaceId,
                                                                   @RequestBody CreateWorkspaceNoticeRequestDto requestDto){
         return workspaceNoticeService.createWorkspaceNotice(workspaceId, requestDto);
+    }
+
+    @Operation(summary = "워크스페이스 공지사항 목록 조회", description = "워크스페이스 공지사항 목록 조회 요청입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "워크스페이스 공지사항 목록 조회 성공")
+    })
+    @GetMapping
+    public FindWorkspaceNoticesResponseDto findWorkspaceNoticesResponseDto(@PathVariable Long workspaceId){
+        return workspaceNoticeService.findWorkspaceNotices(workspaceId);
+    }
+
+    @Operation(summary = "워크스페이스 공지사항 단건 조회", description = "워크스페이스 공지사항 단건 조회 요청입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "워크스페이스 공지사항 단건 조회 성공")
+    })
+    @GetMapping("/{noticeId}")
+    public FindWorkspaceNoticeResponseDto findWorkspace(@PathVariable Long noticeId){
+        return workspaceNoticeService.findWorkspaceNotice(noticeId);
     }
 }
