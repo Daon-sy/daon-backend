a0c1dd554174375a3ebb0be9136c1d45dd5bdba6
sungeunnn <fronteun@naver.com>
2023-12-04 17:44:18 +0900

    ❇️[feat] WorkspaceNoticeService 워크스페이스 목록 조회, 단건 조회 기능 추가 #219
    - findWorkspaceNotices, findWorkspaceNotice 메서드 추가

--- src/main/java/com/daon/backend/task/service/WorkspaceNoticeService.java
+++ src/main/java/com/daon/backend/task/service/WorkspaceNoticeService.java
@@ -1,12 +1,18 @@
 package com.daon.backend.task.service;
 
 import com.daon.backend.task.domain.workspace.*;
+import com.daon.backend.task.dto.WorkspaceNoticeSummary;
 import com.daon.backend.task.dto.workspace.CreateWorkspaceNoticeRequestDto;
 import com.daon.backend.task.dto.workspace.CreateWorkspaceNoticeResponseDto;
+import com.daon.backend.task.dto.workspace.FindWorkspaceNoticeResponseDto;
+import com.daon.backend.task.dto.workspace.FindWorkspaceNoticesResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -37,5 +43,28 @@ public class WorkspaceNoticeService{
 
         Long workspaceNoticeId = workspaceNoticeRepository.save(workspaceNotice).getId();
         return new CreateWorkspaceNoticeResponseDto(workspaceNoticeId);
+    }
+
+    /**
+    * 워크스페이스 공지사항 목록 조회
+    * */
+    public FindWorkspaceNoticesResponseDto findWorkspaceNotices(Long workspaceId){
+        List<WorkspaceNotice> workspaceNotices = workspaceNoticeRepository.findWorkspacesByWorkspaceId(workspaceId);
+
+        return new FindWorkspaceNoticesResponseDto(
+                workspaceNotices.stream()
+                        .map(WorkspaceNoticeSummary::new)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    /**
+     * 워크스페이스 공지사항 단건 조회
+     * */
+    public FindWorkspaceNoticeResponseDto findWorkspaceNotice(Long noticeId){
+        WorkspaceNotice workspaceNotice = workspaceNoticeRepository.findWorkspaceNoticeById(noticeId)
+                .orElseThrow(() -> new WorkspaceNoticeNotFoundException(noticeId));
+
+        return new FindWorkspaceNoticeResponseDto(workspaceNotice);
     }
 }
