b61de795e440ff6e97e8d85f4b734d9966c1cfd0
sungeunnn <fronteun@naver.com>
2023-12-04 17:36:18 +0900

    ❇️[feat] FindWorkspaceNoticesResponse 및 FindWorkspaceNoticeResponse DTO 추가 #219

---
+++ src/main/java/com/daon/backend/task/dto/workspace/FindWorkspaceNoticeResponseDto.java
@@ -0,0 +1,15 @@
+package com.daon.backend.task.dto.workspace;
+
+import com.daon.backend.task.domain.workspace.Workspace;
+import com.daon.backend.task.domain.workspace.WorkspaceNotice;
+import com.daon.backend.task.dto.WorkspaceNoticeSummary;
+import lombok.Getter;
+
+@Getter
+public class FindWorkspaceNoticeResponseDto {
+    private WorkspaceNoticeSummary workspaceNotice;
+
+    public FindWorkspaceNoticeResponseDto(WorkspaceNotice workspaceNotice){
+        this.workspaceNotice = new WorkspaceNoticeSummary(workspaceNotice);
+    }
+}
