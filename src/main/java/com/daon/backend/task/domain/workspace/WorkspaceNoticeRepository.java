e78bd4a45053d2dee06f370642fa2b9831b02d42
sungeunnn <fronteun@naver.com>
2023-12-04 14:35:06 +0900

    🔨[refactor] 불필요한 @Repository 애너테이션 제거 #219

--- src/main/java/com/daon/backend/task/domain/workspace/WorkspaceNoticeRepository.java
+++ src/main/java/com/daon/backend/task/domain/workspace/WorkspaceNoticeRepository.java
@@ -1,7 +1,5 @@
 package com.daon.backend.task.domain.workspace;
 
-import org.springframework.stereotype.Repository;
-
 public interface WorkspaceNoticeRepository {
 
     WorkspaceNotice save(WorkspaceNotice workspaceNotice);
